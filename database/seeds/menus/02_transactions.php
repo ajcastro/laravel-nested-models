24ee4a73e736b61a21b32f857f8a6ddbac1aa817
Arjon Jason Castro <ajcastro29@gmail.com>
2018-04-23 11:08:23 +0800

    Add transactions menu

---
+++ database/seeds/menus/02_transactions.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    [
+        'name' => 'transactions',
+        'text' => 'TRANSACTIONS',
+    ], [
+        'name' => 'transactions.selling',
+        'text' => 'SELLING PRODUCTS',
+    ], [
+        'name' => 'transactions.shopping_cart',
+        'text' => 'SHOPPING CART',
+    ], 
+];
